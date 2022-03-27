a6dfe50f3b2a7d2a44624a54590c92c4140b859f
qcq2530278 <qcq#lrm1314>
2022-03-27 11:53:19 +0800

    代码提交-2022-3-27

---
+++ SJ_pro/src/test/java/com/example/sj_pro/SjProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sj_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
