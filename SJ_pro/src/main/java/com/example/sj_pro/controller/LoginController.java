c87b9810f0e3f789666d8e9604b463ed6537f46f
qcq2530278 <qcq#lrm1314>
2022-03-27 22:35:48 +0800

    代码提交-2022-3-27--甜甜大傻瓜，提交

--- SJ_pro/src/main/java/com/example/sj_pro/controller/LoginController.java
+++ SJ_pro/src/main/java/com/example/sj_pro/controller/LoginController.java
@@ -2,11 +2,28 @@ package com.example.sj_pro.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
-    @GetMapping("/")
-    public String login(){
+    /**
+     * 首页
+     * @return
+     */
+    @GetMapping("/index")
+    public String index(){
        return "/sj_login";
+    }
+
+    /**
+     * 登录
+     * @return
+     */
+    @RequestMapping("/login")
+    @ResponseBody
+    public String login(){
+        System.out.println("欢迎登录，请验证账号和密码");
+        return "222";
     }
 }
